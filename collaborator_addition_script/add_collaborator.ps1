b173db911c56f09c07873e4a53789073ac4e76ed
chen902 <chen902@gmail.com>
2018-01-10 11:26:28 +0100

    changed file suffix

--- collaborator_addition_script/add_collaborator.ps1
+++ collaborator_addition_script/add_collaborator.ps1
@@ -1,6 +1,9 @@
 // Ger repositories
-$repositories = Invoke-WebRequest https://api.github.com/user/repos -Headers @{"Authorization"="token f79d3dbfa3f26e209f44ab8bd6bdef9e66d55e18"} -UseBasicParsing | ConvertFro
-m-Json | %{$_.name}
+$repositories = Invoke-WebRequest https://api.github.com/user/repos -Headers @{"Authorization"="token TOKEN"} -UseBasicParsing | ConvertFro
+m-Json
 
-Invoke-WebRequest -Method PUT https://api.github.com/repos/cts2017ulm/icts1/collaborators/ara-oforiwa -Headers @{"Authorization"="token f79d3dbfa3f26e209f44ab8bd6
-bdef9e66d55e18"} -UseBasicParsing
+foreach($repo in $repositories){
+	Invoke-WebRequest -Method PUT https://api.github.com/repos/cts2017ulm/icts1/collaborators/USERNAME -Headers @{"Authorization"="token TOKEN"} -UseBasicParsing
+}
+
+
